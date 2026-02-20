a08db87e90cad332cbd7ee8611fbcbd0aa6ae8aa
Anass <12345678+yourusername@users.noreply.github.com>
2026-02-20 07:15:55 +0000

    Done

--- Python_Module_01/ex2/ft_plant_growth.py
+++ Python_Module_01/ex2/ft_plant_growth.py
@@ -3,17 +3,17 @@ class Plant:
     def __init__(self, name, height, age):
         self.name = name
         self.height = height
-        self.age = age
+        self.agee = age
         Plant.plants.append(self)
 
-def grow(self):
-    self.height += 6
+    def grow(self):
+        self.height += 6
 
-def age(self):
-    self.age += 6
+    def age(self):
+        self.agee += 6
 
-def get_info(self):
-    return f"{self.name}: {self.height}cm, {self.age} days old"
+    def get_info(self):
+        return f"{self.name}: {self.height}cm, {self.agee} days old"
 
 rose = Plant("rose", 25, 30)
 sunflower = Plant("sunflower", 80, 45)
@@ -25,11 +25,11 @@ if __name__ == "__main__":
         plant.name = plant.name.capitalize()
         print(f"Plant {i}:")
         print("=== Day 1 ===")
-        print(get_info(plant))
-        grow(plant)
-        age(plant)
+        print(plant.get_info())
+        plant.grow()
+        plant.age()
         print("=== Day 7 ===")
-        print(get_info(plant))
+        print(plant.get_info())
         print()
         i += 1
 
