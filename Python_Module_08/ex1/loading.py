9fd4d5589742da6c7dcaa7e77337e977d440fab3
Anass <12345678+yourusername@users.noreply.github.com>
2026-04-18 05:09:30 +0100

    Done

--- Python_Module_08/ex1/loading.py
+++ Python_Module_08/ex1/loading.py
@@ -48,9 +48,9 @@ def run_matrix_analysis() -> None:
 
 
 def compare_package_managers() -> None:
-    print("\n" + "="*62)
+    print("\n" + "=" * 62)
     print(" MATRIX ARCHITECTURE: PIP vs POETRY")
-    print("="*62)
+    print("=" * 62)
 
     print("[pip]: The Drone Installer")
     print("  -> Installs requested packages (requirements.txt)")
@@ -63,7 +63,7 @@ def compare_package_managers() -> None:
     print("     version are suitable together before start to download.")
     print("  -> Uses a 'poetry.lock' file: a 100% mathematically")
     print("     identical snapshot of your system.")
-    print("="*62)
+    print("=" * 62)
 
 
 def main() -> None:
@@ -73,13 +73,13 @@ def main() -> None:
         print("\n[ERROR]: Missing required programs.")
 
         print(" Hint: To load your all packages, run:")
-        print("="*39)
+        print("=" * 39)
         print("$ pip install -r requirements.txt")
-        print("="*35)
+        print("=" * 35)
         print(" or:")
-        print("="*18)
+        print("=" * 18)
         print("$ poetry install")
-        print("="*18)
+        print("=" * 18)
 
     compare_package_managers()
 
---
+++ Python_Module_09/ex0/space_station.py
@@ -0,0 +1,64 @@
+from typing import Optional
+from pydantic import BaseModel, Field, ValidationError
+from datetime import datetime
+
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length=50)
+    crew_size: int = Field(ge=1, le=20)
+    power_level: float = Field(ge=0, le=100)
+    oxygen_level: float = Field(ge=0, le=100)
+    last_maintenance: datetime
+    is_operational: bool = True
+    notes: Optional[str] = Field(default=None, max_length=200)
+
+
+def main() -> None:
+    print("\n     Space Station Data Validation")
+
+    station = SpaceStation(
+        station_id="SS001",
+        name="International Space Station",
+        crew_size=6,
+        power_level=85.5,
+        oxygen_level=92.3,
+        last_maintenance=datetime.fromisoformat("2026-04-18T14:30:00")
+        # I can pass: "2026-04-18 14:30:00" or "2026-04-18"
+        )
+
+    print("=" * 40)
+    print("Valid station created:")
+    print("=" * 40)
+
+    print(f"ID: {station.station_id}")
+    print(f"Name: {station.name}")
+    print(f"Crew: {station.crew_size} people")
+    print(f"Power: {station.power_level}%")
+    print(f"Oxygen: {station.oxygen_level}%")
+
+    if station.is_operational:
+        print("Status: Operational\n")
+    else:
+        print("Status: Not operational\n")
+
+    print("=" * 40)
+    print("Expected validation error:")
+    print("=" * 40)
+
+    try:
+        SpaceStation(
+            station_id="SS001",
+            name="International Space Station",
+            crew_size=30,
+            power_level=85.5,
+            oxygen_level=92.3,
+            last_maintenance=datetime.fromisoformat("2026-04-18")
+            # I can use: "last_maintenance=datetime.fromtimestamp(1776522600)"
+            )
+    except ValidationError as e:
+        print(e, "\n")
+
+
+if __name__ == "__main__":
+    main()
