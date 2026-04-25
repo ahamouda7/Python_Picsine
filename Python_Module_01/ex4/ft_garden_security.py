8e5b4dc985c9cd9a53836adb085d8c69a21072d5
Anass <12345678+yourusername@users.noreply.github.com>
2026-04-26 00:32:22 +0100

    Done

--- Python_Module_01/ex4/ft_garden_security.py
+++ Python_Module_01/ex4/ft_garden_security.py
@@ -1,40 +1,40 @@
 class SecurePlant:
     def __init__(self, name: str, height: int, age: int) -> None:
-        self._name = name
-        self._height = 0
-        self._age = 0
+        self.__name = name
+        self.__height = 0
+        self.__age = 0
 
-        print(f"Plant created: {self._name}")
+        print(f"Plant created: {self.__name}")
 
         self.set_height(height)
         self.set_age(age)
 
     def set_height(self, height: int) -> None:
         if height >= 0:
-            self._height = height
-            print(f"Height updated: {self._height}cm [OK]")
+            self.__height = height
+            print(f"Height updated: {self.__height}cm [OK]")
         else:
             print(f"Invalid operation attempted: height {height}cm [REJECTED]")
             print("Security: Negative height rejected\n")
 
     def set_age(self, age: int) -> None:
         if age >= 0:
-            self._age = age
-            print(f"Age updated: {self._age} days [OK]\n")
+            self.__age = age
+            print(f"Age updated: {self.__age} days [OK]\n")
         else:
             print(f"Invalid operation attempted: age {age} days [REJECTED]")
             print("Security: Negative age rejected\n")
 
     def get_height(self) -> int:
-        return self._height
+        return self.__height
 
     def get_age(self) -> int:
-        return self._age
+        return self.__age
 
 
 if __name__ == "__main__":
     print("=== Garden Security System ===")
     rose = SecurePlant("Rose", 25, 30)
     rose.set_height(-5)
-    print(f"Current plant: {rose._name} "
+    print(f"Current plant: {rose._SecurePlant__name} "
           f"({rose.get_height()}cm, {rose.get_age()} days)")
